b84e5770cd087a055307bbe91ca3b0fccb0e6c2d
Wan Lucas <wan.lucas.90@gmail.com>
2022-09-14 21:27:22 -0300

    :tada: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="attribution">
+        Challenge by
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>
+        .
+        Coded by
+        <a href="#https://github.com/wanlucas">Your Name Here</a>
+        .
+      </div>
+    </div>
+  );
+}
+
+export default App;

865989d4a29141e09c42c984d4a02e39230fdba7
Wan Lucas <wan.lucas.90@gmail.com>
2022-09-25 17:23:43 -0300

    :lipstick: updating app style

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import CommentBar from './components/CommentBar';
 import Message from './components/Message';
 import GlobalContext from './context/globalContext';
+import './App.css';
 
 function App() {
   const { messages } = React.useContext(GlobalContext);
@@ -13,17 +14,21 @@ function App() {
           {messages.map((message) => <Message key={message.id} value={message} />)}
         </ul>
         <section className="add-comment">
-          <CommentBar />
+          <CommentBar type="create" />
         </section>
       </main>
       <footer>
         <div className="attribution">
-          Challenge by
-          <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>
-          .
-          Coded by
-          <a href="#https://github.com/wanlucas">Your Name Here</a>
-          .
+          <p>
+            Challenge by
+            <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>
+            .
+          </p>
+          <p>
+            Coded by
+            <a href="#https://github.com/wanlucas">Wan Lucas</a>
+            .
+          </p>
         </div>
       </footer>
     </div>

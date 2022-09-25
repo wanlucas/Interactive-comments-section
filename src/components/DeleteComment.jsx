d63fed12a0dfcdc33b48ffc1c703475b0e29dead
Wan Lucas <wan.lucas.90@gmail.com>
2022-09-25 17:19:32 -0300

    :lipstick: adding deleteComment style

--- src/components/DeleteComment.jsx
+++ src/components/DeleteComment.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './DeleteComment.css';
 
 function DeleteComment({ modal, del }) {
   return (
-    <div>
+    <div className="delete-message">
       <span className="delete_message-modal">
         <h3>Delete comment</h3>
         <p>
           {
             `Are you sure you want to delete this comment?
-            This will remove the comment and can't be undone`
+            This will remove the comment and can't be undone.`
           }
         </p>
-        <button type="button" onClick={() => modal(false)}>No, cancel</button>
-        <button type="button" onClick={del}>Yes, delete</button>
+
+        <div>
+          <button type="button" className="cancel-btn" onClick={() => modal(false)}>No, cancel</button>
+          <button type="button" className="del-btn" onClick={del}>Yes, delete</button>
+        </div>
       </span>
     </div>
   );
